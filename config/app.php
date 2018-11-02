a9910a627d25410e9e04c8412fb32d1f3ed17df8
Wesley Pettyjohn <w.a.pettyjohn@gmail.com>
2018-11-01 20:34:42 -0400

    Add and configure user auth

--- config/app.php
+++ config/app.php
@@ -160,6 +160,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Passport
+        Laravel\Passport\PassportServiceProvider::class,
+
     ],
 
     /*
--- resources/js/components/Notes.vue
+++ resources/js/components/Notes.vue
@@ -1,29 +1,28 @@
 <template>
   <div>
     <h2>Notes</h2>
+    
     <form @submit.prevent="addNote" class="mb-3">
       <div class="form-group">
         <textarea class="form-control" placeholder="Note" v-model="note.note"></textarea>
       </div>
+
       <div class="form-group">
         <input type="text" class="form-control" placeholder="Color" v-model="note.color">
       </div>
+
       <button type="submit" class="btn btn-light btn-block">Save</button>
     </form>
-    <button @click="clearForm()" class="btn btn-danger btn-block">Cancel</button>
-    <nav aria-label="Page navigation example">
-      <ul class="pagination">
-        <li v-bind:class="[{disabled: !pagination.prev_page_url}]" class="page-item"><a class="page-link" href="#" @click="fetchNotes(pagination.prev_page_url)">Previous</a></li>
-
-        <li class="page-item disabled"><a class="page-link text-dark" href="#">Page {{ pagination.current_page }} of {{ pagination.last_page }}</a></li>
     
-        <li v-bind:class="[{disabled: !pagination.next_page_url}]" class="page-item"><a class="page-link" href="#" @click="fetchNotes(pagination.next_page_url)">Next</a></li>
-      </ul>
-    </nav>
+    <button @click="clearForm()" class="btn btn-danger btn-block">Cancel</button>
+    
     <div class="card card-body mb-2" v-for="note in notes" v-bind:key="note.id">
       <p>{{ note.note }}</p>
+      
       <hr>
+      
       <button @click="editNote(note)" class="btn btn-warning mb-2">Edit</button>
+      
       <button @click="deleteNote(note.id)" class="btn btn-danger">Delete</button>
     </div>
   </div>
@@ -37,11 +36,12 @@ export default {
       note: {
         id:    '',
         note:  '',
-        color: ''
+        color: '',
+        user_id: window.Laravel.userId
       },
-      note_id:    '',
-      pagination: {},
-      edit:       false
+      note_id: '',
+      //pagination: {},
+      edit: false
     };
   },
 
@@ -51,34 +51,20 @@ export default {
 
   methods: {
     fetchNotes(page_url) {
-      let vm = this;
       page_url = page_url || '/api/notes';
-      fetch(page_url)
-        .then(res => res.json())
+      axios.get(page_url)
+        .then(res => res.data)
         .then(res => {
           this.notes = res.data;
-          vm.makePagination(res.meta, res.links);
         })
         .catch(err => console.log(err));
     },
-    makePagination(meta, links) {
-      let pagination = {
-        current_page:  meta.current_page,
-        last_page:     meta.last_page,
-        next_page_url: links.next,
-        prev_page_url: links.prev
-      };
-
-      this.pagination = pagination;
-    },
     deleteNote(id) {
       if (confirm('Are You Sure?')) {
-        fetch(`api/note/${id}`, {
-          method: 'delete'
-        })
-          .then(res => res.json())
+        axios.delete(`api/note/${id}`)
+          //.then(res => res.json())
+          .then(res => res.data)
           .then(data => {
-            alert('Note Removed');
             this.fetchNotes();
           })
           .catch(err => console.log(err));
@@ -86,34 +72,32 @@ export default {
     },
     addNote() {
       if (this.edit === false) {
-        // Add
-        fetch('api/note', {
+        axios({
           method: 'post',
-          body: JSON.stringify(this.note),
+          url: 'api/note',
+          data: JSON.stringify(this.note),
           headers: {
-            'content-type': 'application/json'
+            'Content-Type': 'application/json'
           }
         })
-          .then(res => res.json())
+          .then(res => res.data)
           .then(data => {
             this.clearForm();
-            alert('Note Added');
             this.fetchNotes();
           })
           .catch(err => console.log(err));
       } else {
-        // Update
-        fetch('api/note', {
+        axios({
           method: 'put',
-          body: JSON.stringify(this.note),
+          url: 'api/note',
+          data: JSON.stringify(this.note),
           headers: {
-            'content-type': 'application/json'
+            'Content-Type': 'application/json'
           }
         })
-          .then(res => res.json())
+          .then(res => res.data)
           .then(data => {
             this.clearForm();
-            alert('Note Updated');
             this.fetchNotes();
           })
           .catch(err => console.log(err));
